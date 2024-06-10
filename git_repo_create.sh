aaf12e06095e6619046f31ded085d5031ddd5c9d
DiegoIAV <diego.alfarov@userena.cl>
2024-06-09 21:13:41 -0400

    added comments and auth using PAT as a env var

--- git_repo_create.sh
+++ git_repo_create.sh
@@ -1,19 +1,25 @@
 echo "> Insert repo name:"
 read reponame
+name="DAlfaroV"
 
 mkdir $reponame
 cd $reponame
 
+# Git repo creation with initial commit
 git init
 echo $reponame >> README.md
 git add README.md
-
 git commit -m "initial commit"
 
+# env variables
+source ~/.bashrc
+
+# Auth with github
 gh auth login
 gh repo create $reponame --public
-git remote add origin https://github.com/tu_usuario/$reponame
 
+# push
+git remote add origin https://$github_pat_personal@github.com/$name/$reponame
 git branch -M main
 git push -u origin main
 
